67f249f5c149ec7e10119dee59038c88399a132a
Samuel Billa <98838253+imNewD@users.noreply.github.com>
2026-06-27 16:41:56 +0200

    Delete library directory

--- library/Workout Library/planchepushup-data.js
+++
@@ -1,7 +0,0 @@
-// Planche push-up variants extracted from pushup-data.js
-const planchePushups = [
-if (typeof pushups !== "undefined" && Array.isArray(pushups)) {
-  pushups.push(...planchePushups);
-} else {
-  const pushups = [...planchePushups];
-}
